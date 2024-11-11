3f2b1c1cde3ecb16886879982302bc8f8123fa65
mrauthentik <umokeuchenna2020@gmail.com>
2024-11-11 11:59:29 +0100

    Added more photos ,with some styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
 
 function App() {
   return(
@@ -9,6 +10,7 @@ function App() {
     <div> 
       <Navbar />
       <Hero />
+      <Card />
     </div>
   )
 }
---
+++ my-app/src/components/Card.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Card(){
+    return(
+        <section>
+            <div className='photo-grid'>
+                <div className='card1'>
+                    <img src='/images/photo (1).jpg' alt='' className='card-img'/> 
+                     <div className='details'>
+                         <p> Ratings ⭐⭐</p>
+                         <p> Mario Luiz</p>
+                         <p> 20$</p>
+                     </div>
+                   
+                </div>
+                <div className='card2'> 
+                    <img src='/images/photo (2).jpg' alt='' className='card-img'/>
+                    <div className='details'>
+                        <p> Ratings ⭐⭐⭐⭐⭐</p>
+                        <p> Mario Luiz</p>
+                        <p> 20$</p>
+                    </div> 
+                </div>
+                <div className='card3'>
+                    <img src='/images/photo (3).jpg' alt='' className='card-img'/>
+                    <div className='details'>
+                            <p> Ratings ⭐⭐⭐</p>
+                            <p> Mario Luiz</p>
+                            <p> 20$</p>
+                    </div>
+                </div>
+            </div>
+          
+            
+        </section>
+    )
+}
